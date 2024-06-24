7367710225b69dfd41a174142a32fd7ac75b7a91
Marcos Cruz <123489785+mascoss@users.noreply.github.com>
2024-06-24 18:37:54 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,15 +1,20 @@
 // script.js
 document.addEventListener("DOMContentLoaded", function() {
-    const button = document.getElementById("magicButton");
-    
-    button.addEventListener("click", function() {
+    const noButton = document.getElementById("noButton");
+    const yesButton = document.getElementById("yesButton");
+
+    noButton.addEventListener("click", function() {
         const viewportWidth = window.innerWidth;
         const viewportHeight = window.innerHeight;
         
-        const randomX = Math.random() * (viewportWidth - button.offsetWidth);
-        const randomY = Math.random() * (viewportHeight - button.offsetHeight);
+        const randomX = Math.random() * (viewportWidth - noButton.offsetWidth);
+        const randomY = Math.random() * (viewportHeight - noButton.offsetHeight);
         
-        button.style.left = `${randomX}px`;
-        button.style.top = `${randomY}px`;
+        noButton.style.left = `${randomX}px`;
+        noButton.style.top = `${randomY}px`;
+    });
+
+    yesButton.addEventListener("click", function() {
+        window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"; // Insira o link do vídeo desejado aqui
     });
 });
